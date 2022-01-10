f996aad93b3c855300895bf0ee7aad1a0b20b1fd
ridwannurfaizin12 <ridwannurfaizin12>
2022-01-10 23:01:27 +0700

    Add project files.

---
+++ WcfService_2Way_125/IClientCallback.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace WcfService_2Way_125
+{
+        [ServiceContract]
+        interface IClientCallback
+        {
+            [OperationContract(IsOneWay = true)]
+            void pesanKirim(string user, string pesan);
+        }
+}
---
+++ WcfService_2Way_125/IServiceCallback.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace WcfService_2Way_125
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract(CallbackContract = typeof(IClientCallback))]
+    public interface IServiceCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void gabung(string username);
+        [OperationContract(IsOneWay = true)]
+        void kirimPesan(string pesan);
+    }
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
